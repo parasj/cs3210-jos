7e9e4909976dc03d04d0b1cbd6df4b04f31acd64
Paras Jain <paras@gatech.edu>
2016-11-05 16:06:25 -0400

    Lab 4b ex 8

--- kern/syscall.c
+++ kern/syscall.c
@@ -134,8 +134,12 @@ sys_env_set_status(envid_t envid, int status)
 static int
 sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
-  // LAB 4: Your code here.
-  panic("sys_env_set_pgfault_upcall not implemented");
+  struct Env *env;
+  int envlookup = envid2env(envid, &env, 1);
+  if (envlookup) return envlookup;
+
+  env->env_pgfault_upcall = func;
+  return 0;
 }
 
 // Allocate a page of memory and map it at 'va' with permission
@@ -381,6 +385,9 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
       break;
     case SYS_env_set_status:
       rval = sys_env_set_status((envid_t) a1, a2);
+      break;
+    case SYS_env_set_pgfault_upcall:
+      rval = sys_env_set_pgfault_upcall((envid_t) a1, (void *) a2);
       break;
     case SYS_yield:
       sys_yield();
